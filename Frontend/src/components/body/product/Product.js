0a4ef2017fd02e0ea895b23ef70a715a32363049
dhanuja <dhanujadisanayke@gmail.com>
2024-06-27 23:46:46 +0530

    File size cange

--- Frontend/src/components/body/product/Product.js
+++ Frontend/src/components/body/product/Product.js
@@ -34,6 +34,14 @@ export default function Product() {
         <div class="flex-box"></div>
         <div class="flex-box"></div>
         <div class="flex-box"></div>
+        <div class="flex-box"></div>
+        <div class="flex-box"></div>
+        <div class="flex-box"></div>
+        <div class="flex-box"></div>
+        <div class="flex-box"></div>
+        <div class="flex-box"></div>
+        <div class="flex-box"></div>
+        <div class="flex-box"></div>
       </div>
     </section>
   `;
--- Frontend/src/components/body/product/ProductCss.js
+++ Frontend/src/components/body/product/ProductCss.js
@@ -24,19 +24,19 @@ export default function ProductCss() {
           display: flex;
           justify-content: center;
           align-items: center;
-          overflow: hidden; /* Ensure content doesn't overflow */
-          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Box shadow definition */
-          transition: box-shadow 0.3s ease; /* Smooth transition for hover effect */
+          overflow: hidden; 
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); 
+          transition: box-shadow 0.3s ease; 
         }
   
         .flex-box:hover {
-          box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2); /* Adjusted box shadow on hover */
+          box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2); 
         }
   
         .flex-box img {
           width: 100%;
           height: 100%;
-          object-fit: cover; /* Ensure the image covers the entire box */
+          object-fit: cover; 
         }
       </style>
     `;

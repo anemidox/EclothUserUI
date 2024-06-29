1de428096c7edf87bfefa7d19771c5dabdc1a1a7
dhanuja <dhanujadisanayke@gmail.com>
2024-06-29 21:51:59 +0530

    commit

--- Frontend/src/components/body/loading_page/maincatogory/row_one/one_one_row/oneOneRow.js
+++ Frontend/src/components/body/loading_page/maincatogory/row_one/one_one_row/oneOneRow.js
@@ -8,34 +8,9 @@ export default function oneOneRow() {
 
     return (`
         ${oneOneRowCss()}
-<<<<<<< HEAD
         <div class="one-one-row-container">
             <div class="one-one-row">
                 Edit lskjflskfjsdklfjsk
-=======
-        <div class="shopping-cart-container">
-            <div class="shopping-cart-header">
-                <h2>Your Shopping Cart</h2>
-                <span>${items.length} items</span>
-            </div>
-            <div class="shopping-cart-items">
-                ${items.map(item => `
-                    <div class="shopping-cart-item">
-                        <img class="item-image" src="${item.image}" alt="${item.name}">
-                        <div class="item-details">
-                            <span class="item-name">${item.name}</span>
-                            <span class="item-price">$${item.price.toFixed(2)}</span>
-                            <div class="item-quantity">
-                                <button class="quantity-button" onclick="updateQuantity('${item.name}', -1)">-</button>
-                                <input type="number" class="quantity-input" value="${item.quantity}" min="1" readonly>
-                                <button class="quantity-button" onclick="updateQuantity('${item.name}', 1)">+</button>
-                            </div>
-                            <button class="add-to-cart-button">Add to Cart</button>
-                            <button class="remove-button" onclick="removeItem('${item.name}')">Remove</button>
-                        </div>
-                    </div>
-                `).join('')}
->>>>>>> 9e38f333a55363d23555f0d32e25c3ec8d1b7bca
             </div>
         </div>
 

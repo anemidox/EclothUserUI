04ab0f61fa641ff9a0b00cd2538c4df591331a55
dhanuja <dhanujadisanayke@gmail.com>
2024-07-31 16:08:43 +0530

    create footer

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import header from "./src/common/header.js";
 import { router } from "./router/router.js";
+import footer from "./src/common/footer.js";
 
 const app = () => {
     return `
     <link rel="stylesheet" href="app.css">
         ${header()}
         <div id="app"></div>
+        ${footer()}
     `;
 }
 
---
+++ src/common/footer.js
@@ -0,0 +1,9 @@
+import { Box } from '../components/footer/Box.js';
+
+const footer = () => {
+    return (`
+        <app-box></app-box>
+    `);
+}
+
+export default footer;

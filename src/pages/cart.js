1d0eab378de33877fc02fcea612ea6e714cc9037
dhanuja <dhanujadisanayke@gmail.com>
2024-08-01 14:08:44 +0530

    Cart color changed

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,7 +1,8 @@
+import { Box } from "../components/cart/Box.js";
+
 const cart = () => {
     return (`
-        <div class="cart">
-            <h1>This is cart</h1>
+        <cart-box></cart-box>
     `)
 }
 

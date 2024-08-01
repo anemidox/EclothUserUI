800705a7ea2f0f18dbe9e33202c86921de0fdaa5
dhanuja <dhanujadisanayke@gmail.com>
2024-08-01 18:46:43 +0530

    create button forcart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,8 +1,10 @@
 import { Box } from "../components/cart/Box.js";
+import { Button } from "../components/cart/Button.js";
 
 const cart = () => {
     return (`
         <cart-box></cart-box>
+        <cart-box-button></cart-box-button>
     `)
 }
 

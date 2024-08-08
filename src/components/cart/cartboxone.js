41ef75da9f9153b9197f3ed2e89924fd719cd8c5
MaskINB <168406807+MaskINB@users.noreply.github.com>
2024-08-08 10:25:16 +0530

    Add to new cart

--- src/components/cart/cartboxone.js
+++ src/components/cart/cartboxone.js
@@ -3,9 +3,9 @@ export class cratboxone extends HTMLElement {
       super();
       this.attachShadow({ mode: "open" });
       this.shadowRoot.innerHTML = `
-       <link rel="stylesheet" href="src/components/cart/cartboxone.css">
+       <link rel="stylesheet" href="/src/components/cart/cartboxone.css">
         <div class="cart-item">
-        <img src="/public/icon/images.jpg" alt="Product Image">
+        <img src="/public/icon/logo.png" alt="Product Image">
         <div class="cart-item-details">
           <div class="cart-item-title">Product Title</div>
           <div class="cart-item-price">$19.99</div>

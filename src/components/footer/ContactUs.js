f4d062084ada4c66401d5781e5bdb5ba1d861c9e
Sasidu Samith <hmsskherath20020903@gmail.com>
2024-08-10 00:13:48 +0530

    i created contact us page

--- src/components/footer/ContactUs.js
+++ src/components/footer/ContactUs.js
@@ -4,13 +4,48 @@ export class ContactUs extends HTMLElement {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.innerHTML = `
         <link rel = "stylesheet" href = "src/components/footer/ContactUs.css">
+
             <div class="companylist">
                 <h3>Contact Us</h3>
+
                 <ul>
-                    <li><a href="#/email" onclick="navigateTo('/email')">Email</a></li>
-                    <li><a href="#/phone" onclick="navigateTo('/phone')">Phone No</a></li>
+                    <li><a href="#/email" onclick="navigateTo('/email')">Contact Form</a></li>
+                    <li><a href="#/phone" onclick="navigateTo('/phone')"></a></li>
                 </ul>
+
+
+                <h4> Contact Information </h4>
+                <ul class="contacticon">
+
+                    <li class="contactlist">
+                        <a class="contacta" href="#" target="_blank" rel="noopener noreferrer">
+                        <img class="contactimg" src="public/icon/contacticon/call.png" alt="Call">
+                        </a>
+                    </li>
+
+                    <li class="contactlist">
+                        <a class="contacta" href="#" target="_blank" rel="noopener noreferrer">
+                        <img class="contactimg" src="public/icon/contacticon/whatsapp.png" alt="Whatsapp">
+                        </a>
+                    </li>
+
+                    <li class="contactlist">
+                        <a class="contacta" href="#" target="_blank" rel="noopener noreferrer">
+                        <img class="contactimg" src="public/icon/contacticon/mail.png" alt="Mail">
+                        </a>
+                    </li>
+
+                    <li class="contactlist">
+                        <a class="contacta" href="#" target="_blank" rel="noopener noreferrer">
+                        <img class="contactimg" src="public/icon/contacticon/location.png" alt="Location">
+                        </a>
+                    </li>
+
+                </ul>
+
             </div>
+
+
         `;
     }
 }

223375eae60c1121f4605230e4b662fb827ea04e
animidox <dhanujadisanayke@gmail.com>
2024-12-30 21:45:40 +0530

    Add to comment 2024 Dec 30 my last commit in this year ;

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import { AppHeader } from './src/common/Header.js';
 import { urlLocationHandler } from './src/router/router.js';
-// import { FooterApp } from './src/common/Footer.js';
+// import { FooterApp } from './src/common/Footer.js'; not yet implemented
 
 const app = () => {
     document.body.innerHTML = `
